2d431881eaa2c1fd07a2aa32e59531ee9745e9c1
ysantosengineer <yurisantos594@gmail.com>
2026-09-19 16:24:26 -0300

    feat(db): add product model

--- apps/api/app/models/__init__.py
+++ apps/api/app/models/__init__.py
@@ -2,5 +2,6 @@
 
 from app.models.company import Company
 from app.models.dataset import Dataset
+from app.models.product import Product
 
-__all__ = ["Company", "Dataset"]
+__all__ = ["Company", "Dataset", "Product"]
